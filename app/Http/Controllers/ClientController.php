9dfd3866d8ca24ee36a6fcd03491765744a97b13
sunellopez <sunellopez@gmail.com>
2025-09-04 16:42:08 -0600

    v0.0.0 1.-Commit inicial

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        $data = Client::all();
+
+        return response()->json(['message' => 'Client list', 'data' => $data], 200);
+    }
+}
